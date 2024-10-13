498314c9f7935da777b4e9902116fbe0b678195a
Alexander Bulgakov <asbulgakov90@gmail.com>
2024-10-13 13:48:28 +0300

    Доработки по рекомендациям к дз

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,7 +1,7 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
-import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Question;
 
 @RequiredArgsConstructor
@@ -9,22 +9,34 @@ public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
-    private final CsvQuestionDao csvQuestionDao;
+    private final QuestionDao questionDao;
 
     @Override
     public void executeTest() {
+        printWelcomeMessage();
+        // Получить вопросы из дао и вывести их с вариантами ответов
+        var questions = questionDao.findAll();
+        for (Question question : questions) {
+            printQuestion(question);
+        }
+    }
+
+    private void printWelcomeMessage() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
-        var questions = csvQuestionDao.findAll();
-        for (Question question : questions) {
-            ioService.printLine(question.text());
-            if (question.answers() != null && !question.answers().isEmpty()) {
-                for (int i = 0; i < question.answers().size(); i++) {
-                    ioService.printFormattedLine("%d. %s", i + 1, question.answers().get(i).text());
-                }
-            }
-            ioService.printLine("");
+    }
+
+    private void printQuestion(Question question) {
+        ioService.printLine(question.text());
+        if (question.answers() != null && !question.answers().isEmpty()) {
+            printAnswers(question);
+        }
+        ioService.printLine("");
+    }
+
+    private void printAnswers(Question question) {
+        for (int i = 0; i < question.answers().size(); i++) {
+            ioService.printFormattedLine("%d. %s", i + 1, question.answers().get(i).text());
         }
     }
 }
